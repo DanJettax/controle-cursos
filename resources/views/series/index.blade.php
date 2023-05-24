f07d7303a77e966186e3c5162e8dff28ffcd0d0e
DanJettax <daniel@jettax.com.br>
2023-05-24 08:50:31 -0300

    [dacol] add style css and sidebar

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,10 +1,70 @@
-<x-layout title="Théo">
-    <br>
 
-    <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Inserir Nova Análise</a>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <a href="{{ route('cors.create') }}" class="btn btn-dark mb-2">Inserir Nova Cor</a>
 
-</x-layout>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+        />
+    <link rel="stylesheet" href="../../../css/style.css"/>
+    <title>Document</title>
+</head>
+<body>
+
+<aside class="sidebar">
+    <header class="sidebar-header">
+        <img class="logo-img" src="https://sujeitoprogramador.com/steve.png" alt="foto do usuario">
+    </header>
+
+    <nav>
+        <button>
+            <span>
+                <i class="material-symbols-outlined">login</i>
+                <span>login</span>
+            </span>
+        </button>
+        <button>
+            <span>
+                <i class="material-symbols-outlined">Check_Circle</i>
+                <span>Análises</span>
+            </span>
+        </button>
+        <button>
+            <span>
+                <i class="material-symbols-outlined">Show_chart</i>
+                <span>Gráficos</span>
+            </span>
+        </button>
+        <button>
+            <span>
+                <i class="material-symbols-outlined">person</i>
+                <span>Profile</span>
+            </span>
+        </button>
+        <button>
+            <span>
+                <i class="material-symbols-outlined">Data_Check</i>
+                <span>Tabelas</span>
+            </span>
+        </button>
+    </nav>
+</aside>
+        <main class="main">
+            <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Inserir Nova Análise</a>
+        </main>
+</body>
+</html>
+
+
+
+
+
+
+
+
 
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,5 @@ const mix = require('laravel-mix');
 
 mix
     .sass('resources/css/app.scss', 'public/css');
+
+
